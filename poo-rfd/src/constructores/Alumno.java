31eeb905348dbce319d9e1016d28b471eae912ef
T-102 <T-102@PC244>
2018-01-29 19:15:24 -0600

    Semana 3 Lunes 29 de Enero del 2018 Constructores

---
+++ poo-rfd/src/constructores/Alumno.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+import javax.swing.*; //Crea Pantalallas
+
+public class Alumno {
+    private String nombre;
+    private int edad;
+    private boolean aprobado;
+    
+     public Alumno(int x){
+    
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isAprobado() {
+        return aprobado;
+    }
+
+    public void setAprobado(boolean aprobado) {
+        this.aprobado = aprobado;
+    }
+}
---
+++ poo-rfd/src/constructores/ProbarConstructores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+import javax.swing.JFrame;
+
+public class ProbarConstructores {
+    public static void main(String[] args) {
+         //Alumno a=new Alumno();
+         //System.out.println("Tu edad es "+a.getNombre());
+         int x=2;
+         System.out.println(x);
+         JFrame v=new JFrame("Hola");
+         v.setSize(400, 200);
+         v.setVisible(true);
+                 
+         
+         
+    }
+}
